98a6d0cc72ba240ad1862dff79726ebb2daab46b
გიორგი კახოშვილი <giokaxo@gmail.com>
2018-06-03 16:19:41 +0400

    some improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
@@ -6,74 +7,77 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 // const isProduction = env.production === true;
 const productionPlugins = [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-        template: './index.html',
-        inject: 'body',
-        filename: '../index.html',
-    }),
-    new ExtractTextPlugin('../css/app.css'),
-    new UglifyJSPlugin({
-        sourceMap: true,
-    }),
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('production')
+  }),
+  new CleanWebpackPlugin(['dist']),
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    inject: 'body',
+    filename: '../index.html'
+  }),
+  new ExtractTextPlugin('../css/app.css'),
+  new UglifyJSPlugin({
+    sourceMap: true
+  })
 ];
 
 const developmentPlugins = [
-    new HtmlWebpackPlugin({
-        template: './index.html',
-        inject: 'body',
-    }),
-    new ExtractTextPlugin('app.css'),
+  new HtmlWebpackPlugin({
+    template: './index.html',
+    inject: 'body'
+  }),
+  new ExtractTextPlugin('app.css')
 ];
 
-module.exports = env => {
-    const production = env.production !== undefined;
-    return {
-        entry: path.resolve(__dirname, 'src', 'js', 'app.js'),
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname, 'dist', 'js'),
+module.exports = (env) => {
+  const production = env.production !== undefined;
+  return {
+    entry: path.resolve(__dirname, 'src', 'js', 'app.js'),
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist', 'js')
+    },
+    watch: !production,
+    devtool: 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              outputPath: production ? '../fonts/' : ''
+            }
+          }
         },
-        watch: !production,
-        devtool: 'inline-source-map',
-        module: {
-            rules: [
-                {
-                    test: /\.(woff|woff2|eot|ttf|otf)$/,
-                    use: {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: production ? '../fonts/' : '',
-                        },
-                    },
-                },
-                {
-                    test: /\.js$/,
-                    exclude: /node_modules/,
-                    use: ['babel-loader', 'eslint-loader'],
-                },
-                {
-                    test: /\.scss$/,
-                    use: ExtractTextPlugin.extract({
-                        fallback: 'style-loader',
-                        use: ['css-loader', 'postcss-loader', 'sass-loader'],
-                    }),
-                },
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader'],
-                },
-                {
-                    test: /\.(png|jpg|svg|gif)$/,
-                    use: {
-                        loader: 'file-loader',
-                        options: {
-                            outputPath: production ? '../images/' : '',
-                        },
-                    },
-                },
-            ],
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'eslint-loader']
         },
-        plugins: production ? productionPlugins : developmentPlugins,
-    };
+        {
+          test: /\.scss$/,
+          use: ExtractTextPlugin.extract({
+            fallback: 'style-loader',
+            use: ['css-loader', 'postcss-loader', 'sass-loader']
+          })
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpg|svg|gif)$/,
+          use: {
+            loader: 'file-loader',
+            options: {
+              outputPath: production ? '../images/' : ''
+            }
+          }
+        }
+      ]
+    },
+    plugins: production ? productionPlugins : developmentPlugins
+  };
 };
